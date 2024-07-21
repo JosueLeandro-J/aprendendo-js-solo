d621c1d99e4c5f5bb51b33c309868f1d6f44ebcc
Josué Leandro <josueleandro49@gmail.com>
2024-07-21 11:27:45 -0300

    Create js.js

---
+++ tipos primitivos/js.js
@@ -0,0 +1,26 @@
+// //number
+//_______________________________
+// 1;
+// 1.4;
+// -3;
+// //string
+//_______________________________
+// "olá";
+// 'josué';
+// `bom dia`;//teamplate string
+
+// //boolean
+//_______________________________
+// true or false;
+
+// //null = nada porém diferante de 0
+
+
+// //undefined = um valor indefinido
+//pegando dados
+
+
+
+
+
+
